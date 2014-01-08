98e2c355f3c4f602b44f0ecd40673390ce7d448f
mhemeryck <martijn.hemeryck@soundtalks.com>
2014-01-08 22:40:58 +0100

    set db connections back to production

--- billboy.py
+++ billboy.py
@@ -6,13 +6,13 @@ from flask import (Flask, request, session, redirect, url_for, abort,
 from flask.ext.sqlalchemy import SQLAlchemy
 
 
-DATABASE = '/tmp/billboy.db'
-DEBUG = True
+#DATABASE = '/tmp/billboy.db'
+DEBUG = False
 SECRET_KEY = 'development key'
 USERNAME = 'admin'
 PASSWORD = 'default'
-#SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + DATABASE
+SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+#SQLALCHEMY_DATABASE_URI = 'sqlite:///' + DATABASE
 
 
 app = Flask(__name__)
